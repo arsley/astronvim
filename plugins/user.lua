0e47f18e4e4ee48e95098f3956ac67d342714a8a
Aoshi Fujioka <blue20will@gmail.com>
2023-10-08 17:21:59 +0900

    add nvim-surround

--- plugins/user.lua
+++ plugins/user.lua
@@ -10,4 +10,12 @@ return {
   --   end,
   -- },
   { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({})
+    end
+  },
 }
