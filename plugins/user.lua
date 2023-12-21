cb23b45b0cabfded170164c2ee2203daa92ac819
Aoshi Fujioka <blue20will@gmail.com>
2023-12-22 01:03:52 +0900

    install flutter-tools

--- plugins/user.lua
+++ plugins/user.lua
@@ -18,4 +18,15 @@ return {
       require("nvim-surround").setup({})
     end
   },
+  {
+    'akinsho/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = function()
+      require("flutter-tools").setup {}
+    end
+  }
 }
